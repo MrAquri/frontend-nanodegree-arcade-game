4409ebe740d912bc4e24f7f11b2ae9400f8bb95e
Wojciech Kogucki <wkogucki@gmail.com>
2018-09-24 21:12:51 +0200

    Creaing Player Class, rendering the setup

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    dt = 20;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,14 +26,16 @@ Enemy.prototype.render = function() {
 
 // Now write your own player class
 class Player {
-  constructor() {
+  constructor(x,y) {
     this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
   }
-  update() {
-
+  update(dt) {
+    dt = 20;
   }
   render() {
-
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
   handleInput(){
 
@@ -43,12 +46,12 @@ class Player {
 
 
 // Now instantiate your objects.
-var bug1 = new Enemy(10,150);
-var bug2 = new Enemy(10,250);
-var bug3 = new Enemy(10,350);
+var bug1 = new Enemy(10,145);
+var bug2 = new Enemy(10,230);
+var bug3 = new Enemy(10,60);
 
 let allEnemies = [bug1, bug2, bug3];
-let player;
+let player = new Player (203,405);
 
 
 // This listens for key presses and sends the keys to your
